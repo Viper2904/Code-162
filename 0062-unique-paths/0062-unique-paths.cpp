524a4d546ade8033c9fb60756999fd3793262879
Aadil Hasan <142513544+Viper2904@users.noreply.github.com>
2025-03-13 19:12:08 +0530

    Time: 7 ms (1.19%), Space: 12.2 MB (8.98%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int dp[1000][1000];
+
+    int solve(int m, int n) {
+        if(m == 1 && n == 1){
+             return 1;
+        }
+        if(m <= 0 || n <= 0){
+             return 0;
+        }
+        if(dp[m][n] != -1) {
+            return dp[m][n];
+        }
+        return dp[m][n] = solve(m - 1, n) + solve(m, n - 1);
+    }
+
+    int uniquePaths(int m, int n) {
+        memset(dp, -1, sizeof(dp));
+        return solve(m, n);
+    }
+};
